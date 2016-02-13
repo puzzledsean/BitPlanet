465208b7e67a1c8d9b01d74848e74774fa0d7bcb
Sean Zhang <puzzledsean@gmail.com>
2016-02-13 02:47:49 -0500

    yay we get the data from blockchain API with basic MEAN stack setup

---
+++ app/models/category.server.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Category Schema
+ */
+var CategorySchema = new Schema({
+	// Category model fields   
+	// ...
+});
+
+mongoose.model('Category', CategorySchema);
---
+++ server.js
@@ -0,0 +1,46 @@
+'use strict';
+/**
+ * Module dependencies.
+ */
+var init = require('./config/init')(),
+	config = require('./config/config'),
+	mongoose = require('mongoose'),
+	chalk = require('chalk'),
+	blockchain = require('blockchain.info');
+	console.log('tryna figure out dis blockchain shit');
+	console.log('tyrone thing' + blockchain.blockexplorer.getBlock('00000000000000000174216a0b453298d9a8bfc489f69c2356bc0f8d51d17157').then(function(result){
+		console.log('SUCCESS-----------------------------------');
+		for(var i = 0; i < result.tx.length; i++){
+			console.log(result.tx[i].relayed_by);
+		}
+	}, function(err){
+		console.log(err);
+	}));
+
+/**
+ * Main application entry file.
+ * Please note that the order of loading is important.
+ */
+
+// Bootstrap db connection
+var db = mongoose.connect(config.db, function(err) {
+	if (err) {
+		console.error(chalk.red('Could not connect to MongoDB!'));
+		console.log(chalk.red(err));
+	}
+});
+
+// Init the express application
+var app = require('./config/express')(db);
+
+// Bootstrap passport config
+require('./config/passport')();
+
+// Start the app by listening on <port>
+app.listen(config.port);
+
+// Expose app
+exports = module.exports = app;
+
+// Logging initialization
+console.log('MEAN.JS application started on port ' + config.port);
