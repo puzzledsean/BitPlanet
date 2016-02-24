a189da65597b8a4523faaadd432390e58ff5a30a
Freddie <fv4.programmer@gmail.com>
2016-02-23 22:03:59 -0500

    Add text to DOM for each transaction; only on refresh

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+var DOMParser = require('xmldom').DOMParser;
 var express = require('express');
+
 var app = express();
 
+Socket = require('blockchain.info/socket');
+
+var mySocket = new Socket();
+
+var buf = [], i = 0
+
+var element = [];
+
+var parser = new DOMParser();
+
+mySocket.onTransaction(function(result){
+	//console.log("result " + result.hash);
+	//console.log(result.relayed_by);
+	buf[i++] = '<div id="pings' + i + '"style="height: 5px; width: 5px; border-radius: 100%; color: #fff;">'
+	buf[i++] = "r" + result.hash;
+	buf[i++] = "ip" + result.relayed_by
+	buf[i++] = "</div>";
+	//console.log("buf" + buf.join("");
+	var doc = parser.parseFromString(buf.join(""), "text");
+	element.push(doc);
+	console.log("Element: " + element);
+	//console.log("Transactionn SHOULD be added");
+});
 
 // used to run app locally or via Heroku
 app.set('port', (process.env.PORT || 5000));
@@ -15,7 +40,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/index', {transactions : element});
 });
 
 app.listen(app.get('port'), function() {
